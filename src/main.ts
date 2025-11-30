35ff83a4e4f4b866bd7d066d52893f8c258ae437
Hankasuarus1 <149338936+Hankasuarus1@users.noreply.github.com>
2025-11-30 13:58:05 +0000

    A clickable button (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
+const container = document.createElement("div");
+
+container.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <button id="myButton">❤️</button>
 `;
+
+// Add to page
+document.body.appendChild(container);
+
+// Get reference to button
+const button = document.getElementById("myButton") as HTMLButtonElement;
+
+// Add event listener
+button.addEventListener("click", () => {
+  alert("Button was clicked!");
+});
